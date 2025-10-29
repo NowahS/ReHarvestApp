6d5933160096f51ea4678815328a6c41f096e35f
Kara Cho <chokara037@gmail.com>
2025-10-29 14:16:28 -0400

    sign up and log in now works

--- ReHarvest/src/App.jsx
+++ ReHarvest/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <Container>
         <h1 className="mb-4 fw-bold text-white">ReHarvest</h1>
         <div>
-          <Button as= {Link} to="/login" variant="success">Login</Button>{" "}
+          <Button as= {Link} to="/login" variant="success">Login</Button>
           <Button as= {Link} to="/signup" variant="warning">Sign Up</Button>
         </div>
       </Container>
--- ReHarvest/src/firebase.js
+++ ReHarvest/src/firebase.js
@@ -3,15 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDntK8_FXv0QkIwqxp1poEIlrf7PwR4k28",
-  authDomain: "reharvest-userbase.firebaseapp.com",
-  projectId: "reharvest-userbase",
-  storageBucket: "reharvest-userbase.firebasestorage.app",
-  messagingSenderId: "417242878021",
-  appId: "1:417242878021:web:ddd31c51e3c7b84f1d132c"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+console.log("Firebase API key:", import.meta.env.VITE_FIREBASE_API_KEY);
