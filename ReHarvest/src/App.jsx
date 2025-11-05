f314977902ea04b020bd0e12246107fcab936022
NowahS <stewartnowah@gmail.com>
2025-11-04 21:48:56 -0500

    working home page tab, redirect to blog

--- ReHarvest/src/App.jsx
+++ ReHarvest/src/App.jsx
@@ -6,10 +6,12 @@ import Login from './pages/Login'
 import SignUp from './signup/SignUp'
 import Home from './pages/Home'
 import Opening from './pages/Opening'
+import UserProfile from './userProfile'
 import {Button} from 'react-bootstrap'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+
 
 function App() {
     return(
@@ -20,6 +22,7 @@ function App() {
          <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/home" element={<Home />} />
+          <Route path= "/userprofile" element={<UserProfile />}/>
         </Routes>
       </Router>
 
