aec66ea860326b0fe152b23b1ad9a3d5a0716ef5
KiearaBlackwood <kiearablackwood@gmail.com>
2025-11-09 19:38:07 -0500

    updated file

--- ReHarvest/src/userProfile.jsx
+++ ReHarvest/src/userProfile.jsx
@@ -1,33 +1,29 @@
 import React, { useState } from 'react';
-import {uploadPost} from "./firebasePosts"
-import Col from 'react-bootstrap/Col'
-import Container from 'react-bootstrap/Container'
-import Image from 'react-bootstrap/Image'
-import Row from 'react-bootstrap/Row'
-import Card from 'react-bootstrap/Card'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import ProfilePicture from "./assets/profileIcon.png"
-import Nav from "react-bootstrap/Nav"
-
-
+import { uploadPost } from "./firebasePosts";
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import ProfilePicture from "./assets/profileIcon.png";
+import Nav from "react-bootstrap/Nav";
 
 const UserProfile = () => {
-    const[query, setQuery] = useState(' ')
+    const [query, setQuery] = useState('');
     const [showAddPost, setShowAddPost] = useState(false);
-    const [addPost, setAddPost] = useState({title: "", content: ""});
+    const [addPost, setAddPost] = useState({ title: "", content: "" });
     const [posts, setPosts] = useState([]);
+
     const handleAddPost = async (e) => {
         e.preventDefault();
-        try{
+        try {
             await uploadPost(addPost.title, addPost.content);
-            setPosts([...posts, addPost]);
-            setAddPost({title: "", content: ""});
+            setPosts([...post, addPost]);
+            setAddPost({ title: "", content: "" });
             setShowAddPost(false);
         }
         catch (error){
             console.log(error)
-        }
+        } 
     }
     const [profileData, setProfileData] = useState({
         username: 'Username',
@@ -53,94 +49,90 @@ const UserProfile = () => {
 
     return (
         <>
-        <Nav fill variant="tabs" defaultActiveKey= "/userprofile">
-            <Nav.Item>
-                <Nav.Link href="/home">Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-1">About</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-2">Market</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link href="/userprofile">Blog</Nav.Link>
-            </Nav.Item>
-        </Nav>
-        
-        <Container className="profile-container d-flex justify-content-center">
-            <Row>
-                <Col>
-                    <Card className="profile-card">
-                        <Card.Body className="profile-card-body p-0 text-center">
-            
-                            <div className="mb-5 d-flex justify-content-center">
-                                <Image
-                                    src={ProfilePicture}
-                                    alt="Profile Picture"
-                                    roundedCircle
-                                    className="profile-image"
-                                    style={{ width: '100px', height: '100px', objectFit: 'cover' }}
-                                />
-                            </div>
+            <Nav fill variant="tabs" defaultActiveKey="/userprofile">
+                <Nav.Item>
+                    <Nav.Link href="/home">Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-1">About</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-2">Market</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href="/userprofile">Blog</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <br/>
 
-                            <h2 className="profile-username">@{profileData.username}</h2>
+            <div className="post-container mt-4 p-3 border rounded bg-light">
+                <Image
+                    src={ProfilePicture}
+                    alt="Profile Picture"
+                    roundedCircle
+                    className="profile-image"
+                    style={{ width: '100px', height: '100px', objectFit: 'cover' }}
+                />
 
-                            <div className="text-start">
-                                <Form.Group className="mb-5">
-                                    <Form.Label className="profile-label">Bio</Form.Label>
-                                    <br></br>
-                                    <Form.Control
-                                        as="textarea"
-                                        name="bio"
-                                        rows={10}
-                                        placeholder="Tell us about yourself..."
-                                        value={profileData.bio}
-                                        onChange={handleInputChange} 
-                                        className={`profile-input ${isEditing ? 'editable-input' : ''}`}
-                                    />
-                                </Form.Group>
+                <h2 className="profile-username">@{profileData.username}</h2>
 
-                                <Form.Group className="mb-4">
-                                    <br></br>
-                                    <Form.Label className="profile-label">Links/Socials</Form.Label>
-                                    <br></br>
-                                    <Form.Control
-                                        as="textarea"
-                                        name="socials"
-                                        rows={4}
-                                        placeholder="Enter your socials"
-                                        value={profileData.socials}
-                                        onChange={handleInputChange}
-                                        className={`profile-input ${isEditing ? 'editable-input' : ''}`}
-                                    />
-                                </Form.Group>
-                            </div>
-                        </Card.Body>
+                <div className="text-start">
+                    <Form.Group className="mb-5">
+                        <Form.Label className="profile-label">Bio</Form.Label>
+                        <Form.Control
+                            as="textarea"
+                            name="bio"
+                            rows={4}
+                            placeholder="Tell us about yourself..."
+                            value={profileData.bio}
+                            onChange={handleInputChange}
+                            className={`profile-input ${isEditing ? 'editable-input' : ''}`}
+                            disabled={!isEditing}
+                        />
+                    </Form.Group>
 
-                        <Card.Footer className="profile-card-footer p-4 border-0">
-                            <br></br>
-                            <Button 
-                                variant={isEditing ? "success" : "primary"} 
-                                className="w-100"
-                                onClick={handleEditSave}
-                            >
-                                {isEditing ? "Save Changes" : "Edit Profile"}
-                            </Button>
-                            <br/>
-                            <Button onClick = {() => setShowAddPost(!showAddPost)}>+ Add Post</Button>
-                            {showAddPost && (
-                                <Form onSubmit = {handleAddPost}>
-                                    <Form.Control type="text" placeholder = "Title" value = {addPost.title} onChange = {(e) => setAddPost({...addPost, title: e.target.value})}/>
-                                    <Form.Control as="textarea" placeholder = "Content" value = {addPost.content} onChange = {(e) => setAddPost({...addPost, content: e.target.value})}/>
-                                    <Button type="submit">Submit</Button>
-                                </Form>
-                            )}
-                        </Card.Footer>
-                    </Card>
-                    <h2 className="w-100 text-white">Your Blog Posts</h2>
-                    <div>
-                        {posts.map((post, index) => (
+                    <Form.Group className="mb-4">
+                        <Form.Label className="profile-label">Links/Socials</Form.Label>
+                        <Form.Control
+                            as="textarea"
+                            name="socials"
+                            rows={2}
+                            placeholder="Enter your socials"
+                            value={profileData.socials}
+                            onChange={handleInputChange}
+                            className={`profile-input ${isEditing ? 'editable-input' : ''}`}
+                            disabled={!isEditing}
+                        />
+                    </Form.Group>
+                </div>
+            </div>
+
+            <div className="profile-card-footer p-4 border-0">
+                <Button
+                    variant={isEditing ? "success" : "primary"}
+                    className="w-100"
+                    onClick={handleEditSave}
+                >
+                    {isEditing ? "Save Changes" : "Edit Profile"}
+                </Button>
+
+                <br /><br />
+
+                <Button onClick={() => setShowAddPost(!showAddPost)}>+ Add Post</Button>
+
+                {showAddPost && (
+                    <Form onSubmit = {handleAddPost}>
+                        <Form.Control type="text" placeholder = "Title" value = {addPost.title} onChange = {(e) => setAddPost({...addPost, title: e.target.value})}/>
+                        <Form.Control as="textarea" placeholder = "Content" value = {addPost.content} onChange = {(e) => setAddPost({...addPost, content: e.target.value})}/>
+                        <Button type="submit">Submit</Button>
+                    </Form>
+                )}
+            </div>
+
+            <h2 className="w-100 text-white mt-4">Your Blog Posts</h2>
+
+           <div>
+                {posts.map((post, index) => (
                             <Card key={index}>
                                 <Card.Body>
                                     <Card.Title>{post.title}</Card.Title>
@@ -148,14 +140,10 @@ const UserProfile = () => {
                                     <Card.Text>{post.createdAt}</Card.Text>
                                 </Card.Body>
                             </Card>
-                        ))}
-                    </div>
-                </Col>
-            </Row>
-        </Container>
+                ))}
+            </div> 
         </>
-        
     );
-}
+};
 
 export default UserProfile;
